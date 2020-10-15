ced13739ba693f8ba51a3913ba9e47bccb082b54
MancheskiL <lucas.mancheskicol@gmail.com>
2020-10-15 07:01:34 +0200

    dialog appears, and adds to list

--- lib/screens/list.dart
+++ lib/screens/list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:option_chooser/models/list.dart';
-import 'package:option_chooser/screens/dialog.dart';
 
 class MyList extends StatelessWidget {
   @override
@@ -16,19 +15,7 @@ class MyList extends StatelessWidget {
             onPressed: () {
               // var listAdd = context.read<ListModel>();
               // listAdd.add('test');
-              // return showDialog(
-              //     context: context,
-              //     builder: (BuildContext context) {
-              //       return Dialog(
-              //         shape: RoundedRectangleBorder(
-              //           borderRadius: BorderRadius.circular(50),
-              //         ),
-              //         elevation: 6,
-              //         backgroundColor: Colors.blue[100],
-              //         child: Text('Testing Dialog'),
-              //       );
-              //     });
-              return DialogWithTextField();
+              return _displayDialog(context);
             },
           ),
           IconButton(
@@ -49,6 +36,75 @@ class MyList extends StatelessWidget {
         onPressed: null,
         tooltip: 'Random selection',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  _displayDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          elevation: 6,
+          backgroundColor: Colors.transparent,
+          child: _DialogWithTextField(context),
+        );
+      },
+    );
+  }
+
+  Widget _DialogWithTextField(BuildContext context) {
+    TextEditingController _controller = TextEditingController();
+
+    return Container(
+      height: 250,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 24),
+          Text(
+            'Test Dialog!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 17),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.only(top: 10, bottom: 10, right: 15, left: 15),
+            child: TextField(
+              maxLines: 1,
+              autofocus: false,
+              controller: _controller,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Enter Task Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.pop(context),
+                child: Center(child: Text('Cancel')),
+              ),
+              FlatButton(
+                onPressed: () {
+                  var listAdd = context.read<ListModel>();
+                  listAdd.add('${_controller.value.text}');
+                  Navigator.pop(context);
+                },
+                child: Center(child: Text('Submit')),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
