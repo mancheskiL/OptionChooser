358cb76f20d5a80f15dd740223da36cb4bc3a30c
MancheskiL <lucas.mancheskicol@gmail.com>
2020-10-19 06:24:31 +0200

    notes to self

--- lib/models/list.dart
+++ lib/models/list.dart
@@ -22,6 +22,20 @@ class ListModel extends ChangeNotifier {
     // at this point, seems safe to say any change to this class, must call notifyListener
     notifyListeners();
   }
+
+  void resetItems() {
+    // TODO: set all 'item's to complete=false and finishedDate=null
+    // every day (i.e. when midnight passes)
+    // super duper no idea how to do this yet
+
+    // maybe...save the app launch date and upon next app launch, compare dates
+    // if date are not equal, then it's a new day and we reset
+
+    // must notify List route to uncheck boxes, but we don't want to rebuild records
+    // actually i don't think this will be a problem, reseting list items will not
+    // change records list, so even though that route technically rebuilds
+    // there shouldn't be visible (or logical for that matter) changes
+  }
 }
 
 class Item {
